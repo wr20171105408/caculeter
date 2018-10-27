83d72a5f07866c93d61214cb6dbf14880461a874
wr20171105408 <2394889963@qq.com>
2018-10-27 08:59:02 +0800

    Initial Commit

---
+++ caculeter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  caculeter
+//
+//  Created by s20171105408 on 2018/10/27.
+//  Copyright © 2018年 s20171105408. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ caculeterTests/caculeterTests.swift
@@ -0,0 +1,34 @@
+//
+//  caculeterTests.swift
+//  caculeterTests
+//
+//  Created by s20171105408 on 2018/10/27.
+//  Copyright © 2018年 s20171105408. All rights reserved.
+//
+
+import XCTest
+@testable import caculeter
+
+class caculeterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
